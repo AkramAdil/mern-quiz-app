fb06ef7f119fbf98b10c615d291c0231cd953810
Akrm Adil <akramadil4learn@gmail.com>
2022-04-15 01:58:22 +0200

    using table elemnts instaed of div

--- src/pages/Results/top-table.js
+++ src/pages/Results/top-table.js
@@ -4,22 +4,32 @@ const TopTable = ({topStudents}) => {
     return (
         <div className="top-table">
             <h2 className="table-name">أوئل الطلبة</h2>
-            <div className="tabel-head">
-                <p>المركز</p>
-                <p>الاسم</p>
-                <p>الدرجة</p>
-            </div>
-            
-            {topStudents?
-            topStudents.map((student,i)=>
-                <div className="row" key={student.id}>
-                    <p>{i+1}</p>
-                    <p>{student.name}</p>
-                    <p>{student.endResult}</p>
-                </div>
-                ):"جاري تحميل أوائل الطلبة"
-                
-            }
+            <table>
+                <thead>    
+                    <tr className="tabel-head">
+                        <th>المركز</th>
+                        <th>الاسم</th>
+                        <th>الدرجة</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {topStudents?
+                    topStudents.map((student,i)=>
+                        <tr className="row" key={student.id}>
+                            <td className="place">{i+1}</td>
+                            <td className="userName">{student.name}</td>
+                            <td className="userResult">{student.endResult}</td>
+                        </tr>
+                        ):
+                        <tr className="row">
+                            <td className="place">لجاري التحميل</td>
+                            <td className="userName">جاري التحميل</td>
+                            <td className="userResult">جاري التحميل</td>
+                        </tr>
+                        
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
